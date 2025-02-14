663e21a349ad99b138ba2b3b5ff2e53072e0c6c3
Sunny Gupta <sunnygupta4924@gmail.com>
2025-02-14 11:22:19 +0530

    made my server live

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var figlet = require("figlet");
+const express=require("express");
+const app=express();
+const path=require("path");
 
-figlet("SitaRam !!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"))
+app.listen(8080,()=>{
+    console.log("server is started");
+})
+
+app.get("/home",(req,res)=>{
+    figlet('sitaram!', (err, data) => {
+        if (err) {
+            console.error('Error generating Figlet text:', err);
+            return res.status(500).send('Error generating Figlet text');
+        }
+        // Render the EJS template and pass the Figlet output
+        res.render('home.ejs', { figletOutput: data });
+    });
+})
+
